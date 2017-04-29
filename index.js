529cac4562ebd57acda48dee796bb5f98bf46478
Tevin Mantock <tmantock@gmail.com>
2017-04-29 16:19:23 -0700

    async and promises

--- index.js
+++ index.js
@@ -13,4 +13,48 @@ var me = new Person('John', 'Doe');
 
 me.growOlder(5);
 
+function resolveAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
 
+async function add1(x) {
+  var a = resolveAfter2Seconds(20);
+  var b = resolveAfter2Seconds(30);
+  return x + await a + await b;
+}
+
+add1(10).then(v => {
+  console.log(v);  // prints 60 after 2 seconds.
+});
+
+async function add2(x) {
+  var a = await resolveAfter2Seconds(20);
+  var b = await resolveAfter2Seconds(30);
+  return x + a + b;
+}
+
+add2(10).then(v => {
+  console.log(v);  // prints 60 after 4 seconds.
+});
+
+let myPromise = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        if(!true) {
+            resolve("Promised!");
+        } else {
+            reject("Failed");
+        }
+    }, 1000);
+});
+
+myPromise
+    .then((success) => {
+        console.log(success);
+    })
+    .catch((reason) => {
+        console.log(reason);
+    });
