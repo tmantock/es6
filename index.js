319656c64785571b46323e05665627a58ee2e7e9
Tevin Mantock <tmantock@gmail.com>
2017-04-29 21:16:12 -0700

    yield

--- index.js
+++ index.js
@@ -58,3 +58,15 @@ myPromise
     .catch((reason) => {
         console.log(reason);
     });
+
+function* foo() {
+  var index = 0;
+  while (index <= 2)
+    yield index++;
+}
+
+var iterator = foo();
+console.log(iterator.next()); // { value: 0, done: false }
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: 2, done: false }
+console.log(iterator.next()); // { value: undefined, done: true }
